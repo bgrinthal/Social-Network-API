e5b363ec4a46315857d5a5c52457bdb21b091236
Ben Grinthal <bgrinthal@gmail.com>
2022-07-12 18:32:50 -0400

    thoughts contoller

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,10 +1,8 @@
 const router = require('express').Router();
 const userRoutes = require('./userRoutes');
-// const thoughtRoutes = require('./thoughtRoutes');
+const thoughtRoutes = require('./thoughtRoutes');
 
 router.use('/users', userRoutes);
-// router.use('/thoughts', thoughtRoutes);
+router.use('/thoughts', thoughtRoutes);
 
 module.exports = router;
-
-// Un-comment thoughts.  Commented out for testing
